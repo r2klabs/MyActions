594f5fda5a13cea7e59da8c519c45a8e9a87564d
r2klabs <rob@r2klabs.com>
2021-09-29 14:26:02 -0400

    Updated file to pass linter tests

--- LoopData.java
+++ LoopData.java
@@ -1,12 +1,21 @@
-public class LoopData{
+/**
+ * This is a sample Java program to run through the linter.
+ */
+public class LoopData
+{
 
+	/**
+	 * This method kicks off the program
+	 * @param args
+	 */
 
 	public static void main(String[] args){
 
 
 		int limit = Integer.parseInt(args[0]);
 
-		while(limit>=0){
+		while(limit>=0)
+		{
 
 			System.out.println("Limit: " + limit);	
 			limit--;
