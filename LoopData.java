58098bc6fb33329e0b2eda53f0ad34175fa8194b
r2klabs <rob@r2klabs.com>
2021-09-29 14:35:59 -0400

    Update 4 for the Linter

--- LoopData.java
+++ LoopData.java
@@ -1,14 +1,14 @@
 /**
  * This is a sample Java program to run through the linter.
  */
-public class LoopData{
+public class LoopData {
 
 	/**
 	 * This method kicks off the program.
 	 * @param args - doesn't require arguments.
 	 */
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 
 
 		int limit = Integer.parseInt(args[0]);
@@ -16,12 +16,12 @@ public class LoopData{
 		while ( limit >= 0 )
 		{
 
-			System.out.println("Limit: " + limit);	
+			System.out.println ("Limit: " + limit) ;	
 			limit--;
-		}
+		} 
 		
-		System.out.println("Done!");
+		System.out.println ("Done!") ;
 
-	}//end main
+	} //end main
 
-}//end class
+} //end class
