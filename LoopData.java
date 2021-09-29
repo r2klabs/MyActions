1b01447582d1ced9b4e7d2078663b2b1fb895df6
r2klabs <rob@r2klabs.com>
2021-09-29 14:12:05 -0400

    Initial Commit

---
+++ LoopData.java
@@ -0,0 +1,18 @@
+public class LoopData{
+
+
+	public static void main(String[] args){
+
+
+		int limit = Integer.parseInt(args[0]);
+
+		while(limit>=0){
+
+			System.out.println("Limit: " + limit);	
+			limit--;
+		}
+	
+
+	}//end main
+
+}//end class
